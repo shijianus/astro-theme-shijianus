f6bdc75bfaae8d602009b35b0d2a05d2ea0e7759
shijianus <shijian.us@gmail.com>
2026-04-02 10:43:54 -0800

    ci: configure static export and GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [react(), mdx()],
-
   vite: {
     plugins: [tailwindcss()]
   }
